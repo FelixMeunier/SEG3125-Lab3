a6e07fc2a5c62c54b79027058b8b227ecd1307da
Félix Meunier <fefelix2001@gmail.com>
2020-06-09 22:56:23 -0400

    Version 2 of the website

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -3,63 +3,83 @@
 
 var products = [
 	{
-		name: "Brocoli",
+		name: "Broccoli (Organic)",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
+		category: "fav",
 		price: 1.99
 	},
 	{
 		name: "Bread",
 		vegetarian: true,
 		glutenFree: false,
+		organic: false,
+		category: "pastry",
 		price: 2.35
 	},
 	{
 		name: "Salmon",
 		vegetarian: false,
 		glutenFree: true,
+		organic: false,
+		category: "meat",
 		price: 10.00
 	},
 	{
-		name: "Celeri",
+		name: "Celery",
 		vegetarian: true,
 		glutenFree: true,
+		organic: false,
+		category: "fav",
 		price: 2.99
 	},
 	{
 		name: "Ham",
 		vegetarian: false,
 		glutenFree: true,
+		organic: false,
+		category: "meat",
 		price: 7.97
 	},
 	{
 		name: "Frozen Pizza",
 		vegetarian: false,
 		glutenFree: false,
+		organic: false,
+		category: "frozenfood",
 		price: 4.99
 	},
 	{
-		name: "Lettuce",
+		name: "Lettuce (Organic)",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
+		category: "fav",
 		price: 1.98
 	},
 	{
 		name: "Cheddar Cheese",
 		vegetarian: true,
 		glutenFree: true,
+		organic: false,
+		category: "diary",
 		price: 6.99
 	},
 	{
-		name: "Milk",
+		name: "Milk (Organic)",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
+		category: "diary",
 		price: 6.49
 	},
 	{
-		name: "Carrots",
+		name: "Carrots (Organic)",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
+		category: "fav",
 		price: 1.49
 	}	
 ];
@@ -69,27 +89,52 @@ var products = [
 // given restrictions provided, make a reduced list of products
 // prices should be included in this list, as well as a sort based on price
 
-function restrictListProducts(prods, restrictionVeg, restrictionGlu) {
+function restrictListProducts(prods, restrictionVeg, restrictionGlu, restrictionOrg) {
 	let product_names = [];
 	
 	for (let i=0; i<prods.length; i+=1) {
-		if((restrictionVeg && restrictionGlu) && (prods[i].vegetarian && prods[i].glutenFree)){
+		if((restrictionVeg && restrictionGlu && restrictionOrg) && (prods[i].vegetarian && prods[i].glutenFree && prods[i].organic)){
 			product_names.push(prods[i].name);
 			product_names.push(prods[i].price);
+			product_names.push(prods[i].category);
 		}
-		else if ((restrictionVeg && !restrictionGlu) && (prods[i].vegetarian == true)){
+		else if((restrictionVeg && restrictionGlu && !restrictionOrg) && (prods[i].vegetarian && prods[i].glutenFree)){
 			product_names.push(prods[i].name);
 			product_names.push(prods[i].price);
+			product_names.push(prods[i].category);
 		}
-		else if ((restrictionGlu && !restrictionVeg) && (prods[i].glutenFree == true)){
+		else if((restrictionVeg && !restrictionGlu && restrictionOrg) && (prods[i].vegetarian && prods[i].organic)){
 			product_names.push(prods[i].name);
 			product_names.push(prods[i].price);
+			product_names.push(prods[i].category);
 		}
-		else if(!restrictionVeg && !restrictionGlu){
+		else if((!restrictionVeg && !restrictionGlu && restrictionOrg) && (prods[i].glutenFree && prods[i].organic)){
 			product_names.push(prods[i].name);
 			product_names.push(prods[i].price);
+			product_names.push(prods[i].category);
+		}
+		else if ((restrictionVeg && !restrictionGlu && !restrictionOrg ) && (prods[i].vegetarian == true)){
+			product_names.push(prods[i].name);
+			product_names.push(prods[i].price);
+			product_names.push(prods[i].category);
+		}
+		else if ((restrictionGlu && !restrictionVeg && !restrictionOrg) && (prods[i].glutenFree == true)){
+			product_names.push(prods[i].name);
+			product_names.push(prods[i].price);
+			product_names.push(prods[i].category);
+		}
+		else if ((!restrictionGlu && !restrictionVeg && restrictionOrg) && (prods[i].organic == true)){
+			product_names.push(prods[i].name);
+			product_names.push(prods[i].price);
+			product_names.push(prods[i].category);
+		}
+		else if(!restrictionVeg && !restrictionGlu && !restrictionOrg){
+			product_names.push(prods[i].name);
+			product_names.push(prods[i].price);
+			product_names.push(prods[i].category);
 		}
 	}
+	
 	return product_names;
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,15 @@
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
 
-function openInfo(evt, tabName) {
-
-	// Get all elements with class="tabcontent" and hide them
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
-
-	// Get all elements with class="tablinks" and remove the class "active"
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-
-	// Show the current tab, and add an "active" class to the button that opened the tab
+function openInfo(tabName) {
+	
 	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " active";
+
+}
+
+function closeInfo(tabName) {
+
+	document.getElementById(tabName).style.display = "none";
 
 }
 
@@ -26,54 +18,283 @@ function openInfo(evt, tabName) {
 // generate a checkbox list from a list of products
 // it makes each product name as the label for the checkbos
 
-function populateListProductChoices(slct0, slct1, slct2) {
+function populateListProductChoices(slct0, slct1, slct2, slct3, slct4, slct5, slct6, slct7) {
+	openInfo('Products');
+	closeInfo('Client');
 	var s0 = document.getElementById(slct0);
     var s1 = document.getElementById(slct1);
     var s2 = document.getElementById(slct2);
+	var s3 = document.getElementById(slct3);
+	var s4 = document.getElementById(slct4);
+	var s5 = document.getElementById(slct5);
+	var s6 = document.getElementById(slct6);
+	var s7 = document.getElementById(slct7);
 	
 	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
-    s2.innerHTML = "";
+    s3.innerHTML = "";
 		
 	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s0.checked, s1.checked);
+    var optionArray = restrictListProducts(products, s0.checked, s1.checked, s2.checked);
 
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
 	// <label for="Bread">Bread/label><br>
-	
-	
+	favArray = [];
+	meatArray = [];
+	diaryArray = [];
+	frozenFoodArray = [];
+	pastryArray = [];
 	while(optionArray.length != 0){
+		for (i = 0; i < optionArray.length; i=i+3){
+			switch(optionArray[i+2]){
+				case "fav":
+					favArray.push(optionArray[i]);
+					favArray.push(optionArray[i+1]);
+					optionArray.splice(i, 3);
+					break;
+				case "meat":
+					meatArray.push(optionArray[i]);
+					meatArray.push(optionArray[i+1]);
+					optionArray.splice(i, 3);
+					break;
+				case "diary":
+					diaryArray.push(optionArray[i]);
+					diaryArray.push(optionArray[i+1]);
+					optionArray.splice(i, 3);
+					break;
+				case "frozenfood":
+					frozenFoodArray.push(optionArray[i]);
+					frozenFoodArray.push(optionArray[i+1]);
+					optionArray.splice(i, 3);
+					break;
+				case "pastry":
+					pastryArray.push(optionArray[i]);
+					pastryArray.push(optionArray[i+1]);
+					optionArray.splice(i, 3);
+					break;
+			}
+		}
+	}
+	
+	
+	//Fruits and vegetables
+
+	while(favArray.length != 0){		
+		
 		var minvalue = 1000;
 		var minint = 0;
 	
-		for (j = 0; j < optionArray.length; j=j+2) {
-			if(optionArray[j+1] < minvalue){
-				minvalue = optionArray[j+1];
+		for (j = 0; j < favArray.length; j=j+2) {
+			if(favArray[j+1] < minvalue){
+				minvalue = favArray[j+1];
 				minint = j;
 			}
 		}
 	
 			
-		var productName = optionArray[minint] + " : " + optionArray[minint+1] + "$";
+		var productName = favArray[minint] + " : " + favArray[minint+1] + "$";
 			// create the checkbox and add in HTML DOM
 			var checkbox = document.createElement("input");
 			checkbox.type = "checkbox";
 			checkbox.name = "product";
 			checkbox.value = productName;
-			s2.appendChild(checkbox);
+			s3.appendChild(checkbox);
 			
 			// create a label for the checkbox, and also add in HTML DOM
 			var label = document.createElement('label')
 			label.htmlFor = productName;
 			label.appendChild(document.createTextNode(productName));
-			s2.appendChild(label);
+			s3.appendChild(label);
 			
 			// create a breakline node and add in HTML DOM
-			s2.appendChild(document.createElement("br"));    
+			s3.appendChild(document.createElement("br")); 
 			
-			optionArray.splice(minint, 2);
+			// create an image for the checkbox, and also add in HTML DOM
+			var img = document.createElement('img')
+			img.src = "D:/GitHub/SEG3125-Lab3/images/"+productName.replace(/[^A-Za-z]/g, "")+".jpg"
+			s3.appendChild(img);
+			
+			// create 2 breakline nodes and add in HTML DOM
+			s3.appendChild(document.createElement("br")); 
+			s3.appendChild(document.createElement("br"));			
+			
+			favArray.splice(minint, 2);
 		
 	}
+	
+	//Meat
+	while(meatArray.length != 0){
+		var minvalue = 1000;
+		var minint = 0;
+	
+		for (j = 0; j < meatArray.length; j=j+2) {
+			if(meatArray[j+1] < minvalue){
+				minvalue = meatArray[j+1];
+				minint = j;
+			}
+		}
+	
+			
+		var productName = meatArray[minint] + " : " + meatArray[minint+1] + "$";
+			// create the checkbox and add in HTML DOM
+			var checkbox = document.createElement("input");
+			checkbox.type = "checkbox";
+			checkbox.name = "product";
+			checkbox.value = productName;
+			s4.appendChild(checkbox);
+			
+			// create a label for the checkbox, and also add in HTML DOM
+			var label = document.createElement('label')
+			label.htmlFor = productName;
+			label.appendChild(document.createTextNode(productName));
+			s4.appendChild(label);
+			
+			// create a breakline node and add in HTML DOM
+			s4.appendChild(document.createElement("br")); 
+			
+			// create an image for the checkbox, and also add in HTML DOM
+			var img = document.createElement('img')
+			img.src = "D:/GitHub/SEG3125-Lab3/images/"+productName.replace(/[^A-Za-z]/g, "")+".jpg"
+			s4.appendChild(img);
+			
+			// create 2 breakline nodes and add in HTML DOM
+			s4.appendChild(document.createElement("br")); 
+			s4.appendChild(document.createElement("br"));			
+			
+			meatArray.splice(minint, 2);
+		
+	}
+	
+	
+	//Diary
+	while(diaryArray.length != 0){
+		var minvalue = 1000;
+		var minint = 0;
+	
+		for (j = 0; j < diaryArray.length; j=j+2) {
+			if(diaryArray[j+1] < minvalue){
+				minvalue = diaryArray[j+1];
+				minint = j;
+			}
+		}
+	
+			
+		var productName = diaryArray[minint] + " : " + diaryArray[minint+1] + "$";
+			// create the checkbox and add in HTML DOM
+			var checkbox = document.createElement("input");
+			checkbox.type = "checkbox";
+			checkbox.name = "product";
+			checkbox.value = productName;
+			s5.appendChild(checkbox);
+			
+			// create a label for the checkbox, and also add in HTML DOM
+			var label = document.createElement('label')
+			label.htmlFor = productName;
+			label.appendChild(document.createTextNode(productName));
+			s5.appendChild(label);
+			
+			// create a breakline node and add in HTML DOM
+			s5.appendChild(document.createElement("br")); 
+			
+			// create an image for the checkbox, and also add in HTML DOM
+			var img = document.createElement('img')
+			img.src = "D:/GitHub/SEG3125-Lab3/images/"+productName.replace(/[^A-Za-z]/g, "")+".jpg"
+			s5.appendChild(img);
+			
+			// create 2 breakline nodes and add in HTML DOM
+			s5.appendChild(document.createElement("br")); 
+			s5.appendChild(document.createElement("br"));			
+			
+			diaryArray.splice(minint, 2);
+		
+	}
+	
+	//Frozen Food
+	while(frozenFoodArray.length != 0){
+		var minvalue = 1000;
+		var minint = 0;
+	
+		for (j = 0; j < frozenFoodArray.length; j=j+2) {
+			if(frozenFoodArray[j+1] < minvalue){
+				minvalue = frozenFoodArray[j+1];
+				minint = j;
+			}
+		}
+	
+			
+		var productName = frozenFoodArray[minint] + " : " + frozenFoodArray[minint+1] + "$";
+			// create the checkbox and add in HTML DOM
+			var checkbox = document.createElement("input");
+			checkbox.type = "checkbox";
+			checkbox.name = "product";
+			checkbox.value = productName;
+			s6.appendChild(checkbox);
+			
+			// create a label for the checkbox, and also add in HTML DOM
+			var label = document.createElement('label')
+			label.htmlFor = productName;
+			label.appendChild(document.createTextNode(productName));
+			s6.appendChild(label);
+			
+			// create a breakline node and add in HTML DOM
+			s6.appendChild(document.createElement("br")); 
+			
+			// create an image for the checkbox, and also add in HTML DOM
+			var img = document.createElement('img')
+			img.src = "D:/GitHub/SEG3125-Lab3/images/"+productName.replace(/[^A-Za-z]/g, "")+".jpg"
+			s6.appendChild(img);
+			
+			// create 2 breakline nodes and add in HTML DOM
+			s6.appendChild(document.createElement("br")); 
+			s6.appendChild(document.createElement("br"));			
+			
+			frozenFoodArray.splice(minint, 2);
+		
+	}
+	
+	//Pastry
+	while(pastryArray.length != 0){
+		var minvalue = 1000;
+		var minint = 0;
+	
+		for (j = 0; j < pastryArray.length; j=j+2) {
+			if(pastryArray[j+1] < minvalue){
+				minvalue = pastryArray[j+1];
+				minint = j;
+			}
+		}
+	
+			
+		var productName = pastryArray[minint] + " : " + pastryArray[minint+1] + "$";
+			// create the checkbox and add in HTML DOM
+			var checkbox = document.createElement("input");
+			checkbox.type = "checkbox";
+			checkbox.name = "product";
+			checkbox.value = productName;
+			s7.appendChild(checkbox);
+			
+			// create a label for the checkbox, and also add in HTML DOM
+			var label = document.createElement('label')
+			label.htmlFor = productName;
+			label.appendChild(document.createTextNode(productName));
+			s7.appendChild(label);
+			
+			// create a breakline node and add in HTML DOM
+			s7.appendChild(document.createElement("br")); 
+			
+			// create an image for the checkbox, and also add in HTML DOM
+			var img = document.createElement('img')
+			img.src = "D:/GitHub/SEG3125-Lab3/images/"+productName.replace(/[^A-Za-z]/g, "")+".jpg"
+			s7.appendChild(img);
+			
+			// create 2 breakline nodes and add in HTML DOM
+			s7.appendChild(document.createElement("br")); 
+			s7.appendChild(document.createElement("br"));			
+			
+			pastryArray.splice(minint, 2);
+		
+	}
+	
 }
 	
 // This function is called when the "Add selected items to cart" button in clicked
@@ -81,6 +302,9 @@ function populateListProductChoices(slct0, slct1, slct2) {
 // We build a paragraph to contain the list of selected items, and the total price
 
 function selectedItems(){
+	
+	openInfo('Cart');
+	closeInfo('Products');
 	
 	var ele = document.getElementsByName("product");
 	var chosenProducts = [];
@@ -102,6 +326,56 @@ function selectedItems(){
 		
 	// add paragraph and total price
 	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts) + "$"));
+	c.appendChild(document.createTextNode("The Total Price is " + getTotalPrice(chosenProducts) + "$"));
 		
+}
+
+function start(){
+	openInfo('Client');
+	closeInfo('Products');
+}
+
+function backToProducts(){
+	openInfo('Products');
+	closeInfo('Cart');
+}
+
+function fav(){
+	if(document.getElementById("displayFav").style.display == "block"){
+		document.getElementById("displayFav").style.display = "none";
+	}else{
+		document.getElementById("displayFav").style.display = "block";
+	}
+}
+
+function meat(){
+	if(document.getElementById("displayMeat").style.display == "block"){
+		document.getElementById("displayMeat").style.display = "none";
+	}else{
+		document.getElementById("displayMeat").style.display = "block";
+	}
+}
+
+function diary(){
+	if(document.getElementById("displayDiary").style.display == "block"){
+		document.getElementById("displayDiary").style.display = "none";
+	}else{
+		document.getElementById("displayDiary").style.display = "block";
+	}
+}
+
+function frozenFood(){
+	if(document.getElementById("displayFrozenFood").style.display == "block"){
+		document.getElementById("displayFrozenFood").style.display = "none";
+	}else{
+		document.getElementById("displayFrozenFood").style.display = "block";
+	}
+}
+
+function pastry(){
+	if(document.getElementById("displayPastry").style.display == "block"){
+		document.getElementById("displayPastry").style.display = "none";
+	}else{
+		document.getElementById("displayPastry").style.display = "block";
+	}
 }
